bb72cb2c257b4e30b34f7170ee522bd25841f974
Cosmin <cosmin.matei@eminescubucuresti.ro>
2022-02-24 17:44:39 +0200

    New

---
+++ V2 Desktop+Mobile (fmm veatz)/script.js
@@ -0,0 +1,19 @@
+const animatie = () =>{
+    const meniu = document.querySelector('.meniu');
+    const nav = document.querySelector('.nav-link');
+    const navLink = document.querySelectorAll('.nav-link li')
+
+   meniu.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLink.forEach((link,index) => {
+        if(link.style.animation){
+            link.style.animation = ``;
+        } else {
+            link.style.animation = `fade 0.5s ease forwards ${index/7 + 0.4}s`
+        }
+
+        });
+        meniu.classList.toggle('toggle');
+   });
+}
+animatie();
