55a5ae24df6933fb1dd592556b9b1f7ef7fa603f
Cosmin <cosmin.matei@eminescubucuresti.ro>
2022-04-15 17:16:44 +0300

    Summary

--- Invitatie Diana V3/script.js
+++ Invitatie Diana V3/script.js
@@ -7,7 +7,7 @@ function animatie() {
     const imagine = document.querySelector('.imagine');
     const text = document.querySelector('.text');
     const data = document.querySelector('.data')
-        const start = () => {
+    const start = () => {
         setTimeout(function() {
             confetti.start()
         }, 4500); 
@@ -17,31 +17,25 @@ function animatie() {
             confetti.stop()
         }, 8000); 
     };
-//de adugat functionalitara cu imaginea
-        imagine.addEventListener('click', () => {
-            //partea 1 (se deschide plicul)
-            imagine.classList.toggle('opac');
-            capac1.classList.toggle('toggle');
-            capac2.classList.toggle('toggle');
-            invitatie.classList.toggle('toggle');
-            invelitoare.classList.toggle('toggle');
-            //partea 2 (dispare plicul)
-            capac1.classList.toggle('opac');
-            capac2.classList.toggle('opac');
-            plic.classList.toggle('opac');
-            invelitoare.classList.toggle('opac');
-            //apare invitatia cu informatii
-            start();
-            stop();
-            invitatie.classList.toggle('marire');
-            text.classList.toggle('opac');
-            data.classList.toggle('opac');
-            
-
-        });
+//de adugat functionalitara cu imaginea    => *rezolvat*
+    imagine.addEventListener('click', () => {
+//partea 1 (se deschide plicul)
+        imagine.classList.toggle('opac');
+        capac1.classList.toggle('toggle');
+        capac2.classList.toggle('toggle');
+        invitatie.classList.toggle('toggle');
+        invelitoare.classList.toggle('toggle');
+//partea 2 (dispare plicul)
+        capac1.classList.toggle('opac');
+        capac2.classList.toggle('opac');
+        plic.classList.toggle('opac');
+        invelitoare.classList.toggle('opac');
+//apare invitatia cu informatii + confetti dupa 0.5s
+        start();
+        stop();
+        invitatie.classList.toggle('marire');
+        text.classList.toggle('opac');
+        data.classList.toggle('opac');
+    });
 }
-
-
-
-
 animatie();
